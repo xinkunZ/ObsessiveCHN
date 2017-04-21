84cdfa6cc019fec4ee11ba88d699030e34bca4b0
zhangxinkun <zxk844977406@outlook.com>
2017-04-21 13:29:58 +0800

    替换成功

--- extension.js
+++ extension.js
@@ -9,49 +9,74 @@ function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "obsessivechn" is now active!');
-
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    // var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-    //     The code you place here will be executed every time your command is executed
-
-    //     Display a message box to the user
-    //     vscode.window.showInformationMessage('Hello World!');
-    // });
-
-    var dis1 = vscode.commands.registerCommand('extension.obsessiveSelected', replaceSelected());
-    var dis2 = vscode.commands.registerCommand('extension.obsessiveAll', replaceAll());
+   
+    var dis1 = vscode.commands.registerCommand('extension.obsessiveAll', replace());
 
     context.subscriptions.push(dis1);
-    context.subscriptions.push(dis2);
 }
 
-function replaceSelected() {
-    if (isAnythingSelected()) {
-        var editor = vscode.window.activeTextEditor;
-        var selection = editor.selection;
-        var newText = regReplace(editor.document.getText(selection));
-        return editor.edit((edit) => {
-            edit.replace(selection, newText)
-        });
-    }
-}
-
-
-function replaceAll() {
+function replace() {
     var editor = vscode.window.activeTextEditor;
     var selection = editor.selection;
-    var newText = regReplace(editor.document.getText());
-    return editor.edit((edit) => {
-        edit.replace(selection, newText)
-    });
+    var startPattern = ' ';
+    var endPattern = ' ';
+    var wordPattern = /([\u4e00-\u9fa5]*)([a-z,A-Z]+)([\u4e00-\u9fa5]+)/g;
+
+    if (!isAnythingSelected()) {
+        var withSurroundingWord = getSurroundingWord(editor, selection, wordPattern);
+
+        if (withSurroundingWord != null) {
+            selection = editor.selection = withSurroundingWord;
+        }
+
+    }
+
+    if (!isAnythingSelected()) {
+        var position = selection.active;
+        var newPosition = position.with(position.line, position.character + startPattern.length)
+        return editor.edit((edit) => {
+            edit.insert(selection.start, startPattern + endPattern);
+        }).then(() => {
+            editor.selection = new vscode.Selection(newPosition, newPosition)
+        })
+    } else if (isSelectionMatch(selection, startPattern, endPattern)) {
+        return replaceSelection((text) => text.substr(startPattern.length, text.length - startPattern.length - endPattern.length))
+    }
+    else {
+        return replaceSelection((text) => {
+            return text.replace(wordPattern, '$1 $2');
+        })
+    }
+
 }
 
-function regReplace(str) {
-    var newvalue = str.replace(new RegExp('([a-z,A-Z])([\u4e00-\u9fa5])', 'gm'), '$1 $2');
-    return newvalue.replace(new RegExp('([\u4e00-\u9fa5])([a-z,A-Z])', 'gm'), '$1 $2');
+function isSelectionMatch(selection, startPattern, endPattern) {
+    var editor = vscode.window.activeTextEditor;
+    var text = editor.document.getText(selection)
+    if (startPattern.constructor === RegExp) {
+        return startPattern.test(text);
+    }
+
+    return text.startsWith(startPattern) && text.endsWith(endPattern)
+}
+
+
+function replaceSelection(replaceFunc) {
+    var editor = vscode.window.activeTextEditor;
+    var selection = editor.selection;
+
+    var newText = replaceFunc(editor.document.getText(selection));
+    return editor.edit((edit) => {
+        edit.replace(selection, newText)
+    })
+}
+
+
+function getSurroundingWord(editor, selection, wordPattern) {
+    var range = editor.document.getWordRangeAtPosition(selection.active, wordPattern);
+    return range == null
+        ? null
+        : new vscode.Selection(range.start, range.end);
 }
 
 function isAnythingSelected() {
